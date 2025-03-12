29f6d703a57e27c5bc965be4134eec4dd9954e43
Srikrishnan <ksri2035@gmail.com>
2025-03-12 11:02:53 +0530

    created a variable string and integer and print the variable using printf

--- main.go
+++ main.go
@@ -3,4 +3,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+	var name string = "John Doe"
+	var age int = 25
+	var name1 string = "Jane Doe"
+	var age1 int = 30
+	fmt.Printf("Name:%s\nAge:%d\nname1:%s\nAge1:%d\n", name1, age, name, age1)
 }
